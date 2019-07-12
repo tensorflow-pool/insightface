305aed252bee1d43bb41a342bc7648ea04ed0836
lijc08 <lijc08@ubuntu-fast.com>
2019-07-12 20:34:27 +0800

    fix

--- src/train_softmax.py
+++ src/train_softmax.py
@@ -563,7 +563,7 @@ def train_net(args):
         if mbatch % args.check_save == 0:
             acc_list = ver_test(mbatch)
             logging.info('[%d]Accuracy-Highest: %s' % (mbatch, acc_list))
-            sw.add_scalar(tag='acc', value=acc_list[0], global_step=global_step[0])
+            sw.add_scalar(tag='val', value=acc_list[0], global_step=global_step[0])
 
             logging.info('saving %s', mbatch)
             arg, aux = model.get_params()
