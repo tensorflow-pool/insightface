17a549f9cefddaaf4671ea068615c40b87e46c9b
lijc08 <lijc08@vanke.com>
2019-12-07 23:51:41 +0800

    fix

--- src/train_softmax_v5.py
+++ src/train_softmax_v5.py
@@ -469,7 +469,7 @@ def train_net(args):
         args.gamma = 0.06
 
     data_shape = (args.image_channel, image_size[0], image_size[1])
-    dataset = FaceDataset(args.leveldb_path, args.label_path, min_images=3000, max_images=10, ignore_labels={0})
+    dataset = FaceDataset(args.leveldb_path, args.label_path, min_images=1000, max_images=10, ignore_labels={0})
     train_dataiter = FaceImageIter(
         batch_size=args.batch_size,
         data_shape=data_shape,
@@ -658,13 +658,13 @@ def train_net(args):
         # 10 100w
         # 50 310-350w
         # 100 500w
+            logging.info("================>epoch_cb epoch %s g_step %s", epoch, global_step[0])
         if epoch == base_lr_steps[-2]:
             logging.info("================>change max_images to 50 epoch %s g_step %s", epoch, global_step[0])
             dataset.max_images = 50
         if epoch == base_lr_steps[-1]:
             logging.info("================>change max_images to 300 epoch %s g_step %s", epoch, global_step[0])
             dataset.max_images = 300
-
 
     train_dataiter = mx.io.PrefetchingIter(train_dataiter)
 
