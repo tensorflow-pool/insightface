9d1da25c7c4a2900f8977f986863b7a131b93a0e
lijc08 <lijc08@vanke.com>
2019-12-14 00:08:13 +0800

    fix

--- src/train_softmax_v5.py
+++ src/train_softmax_v5.py
@@ -134,7 +134,7 @@ def parse_args():
     target = os.path.expanduser("~/datasets/maysa/lfw.bin")
     parser.add_argument('--target', type=str, default=target, help='verification targets')
 
-    parser.add_argument('--load_weight', type=int, default=0, help='重新加载feature')
+    parser.add_argument('--load_weight', type=int, default=1, help='重新加载feature')
     parser.add_argument('--lr', type=float, default=0.01, help='start learning rate')
     parser.add_argument('--per_batch_size', type=int, default=48, help='batch size in each context')
 
@@ -146,8 +146,8 @@ def parse_args():
     # parser.add_argument('--pretrained', default='../models_retina100_2019-10-18/model,486201', help='pretrained model to load')
     # parser.add_argument('--pretrained', default='./train/models_2019-11-06-14:24:12/model,492590', help='pretrained model to load')
     # parser.add_argument('--pretrained', default='./train/models_2019-12-05-21:08:10/model,70060', help='pretrained model to load')
-    # parser.add_argument('--pretrained', default='./train/models_2019-12-06-09:51:21/model,231021', help='pretrained model to load')
-    parser.add_argument('--pretrained', default='', help='pretrained model to load')
+    parser.add_argument('--pretrained', default='./train/models_2019-12-06-09:51:21/model,231021', help='pretrained model to load')
+    # parser.add_argument('--pretrained', default='', help='pretrained model to load')
     parser.add_argument('--loss_type', type=int, default=4, help='loss type 5的时候为cos(margin_a*θ+margin_m) - margin_b;cos(θ+0.3)-0.2 or cos(θ+0.5)')
     parser.add_argument('--max_steps', type=int, default=0, help='max training batches')
     parser.add_argument('--network', default='r100', help='specify network')
