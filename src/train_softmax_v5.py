c423d8626ac0ab5098fe6eb686d49295258f0fc1
lijc08 <lijc08@vanke.com>
2019-12-22 18:42:01 +0800

    fix

--- src/train_softmax_v5.py
+++ src/train_softmax_v5.py
@@ -149,7 +149,7 @@ def parse_args():
     # parser.add_argument('--pretrained', default='./train/models_2019-12-05-21:08:10/model,70060', help='pretrained model to load')
     # parser.add_argument('--pretrained', default='./train/models_2019-12-12-23:04:29/model,9', help='pretrained model to load')
     # parser.add_argument('--pretrained', default='./train/v26_2019-12-18-21:18:18/model,4', help='pretrained model to load')
-    parser.add_argument('--pretrained', default='./train/v28_2019-12-19-23:06:25/model,9', help='pretrained model to load')
+    parser.add_argument('--pretrained', default='./train/v18_2019-12-19-23:06:25/model,9', help='pretrained model to load')
     # parser.add_argument('--pretrained', default='', help='pretrained model to load')
     parser.add_argument('--loss_type', type=int, default=4, help='loss type 5的时候为cos(margin_a*θ+margin_m) - margin_b;cos(θ+0.3)-0.2 or cos(θ+0.5)')
     parser.add_argument('--max_steps', type=int, default=0, help='max training batches')
